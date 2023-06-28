00c395ba68f9413eb8e74f75d149010de3a08b2d
Julio Mafra <julioclmafra@gmail.com>
2023-06-28 15:38:34 -0300

    removendo código desnecessário

--- app/public/index.php
+++ app/public/index.php
@@ -2,10 +2,6 @@
 
 include_once '../../vendor/autoload.php';
 
-use agenda\app\helpers\DataFormat\DataPadraoBR;
-use agenda\app\models\agenda\Agenda;
-use agenda\app\models\agenda\CriarAgenda;
 use agenda\app\routes\ControllerRoutes;
 
 (new ControllerRoutes)->process();
-
